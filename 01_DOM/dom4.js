4d3541a05bcb83410d686c99f461a2e41014d9a6
keshav31 <keshavchauhangtx2004@gmail.com>
2024-12-11 15:52:22 +0530

    learning How to edit in element in js

--- 01_DOM/dom4.js
+++ 01_DOM/dom4.js
@@ -6,9 +6,20 @@ function addLanguage(langName){
 addLanguage("Python")
 addLanguage("typescript");
 function addOptiLanguage(langName){
-    const li=document.createElement('li')
-    const addText=document.createTextNode(langName)
-    li.appendChild(addText)
-    document.querySelector('.language').appendChild(li)
+    const li=document.createElement('li') //create an list element li
+    const addText=document.createTextNode(langName) //take language Name as addText 
+    li.appendChild(addText) //append addtext in li element
+    document.querySelector('.language').appendChild(li) // attching that element in ul className(language) 
 }
 addOptiLanguage("GOLang")
+//more optimise than the previous one as no need to traverse in all tree just appendchild inside the element
+
+//Edit
+const secondLang=document.querySelector("li:nth-child(2)")
+// secondLang.innerHTML="Mongo"
+const newli=document.createElement('li')
+newli.textContent="manga db"
+secondLang.replaceWith (newli)
+//edit
+const first=document.querySelector('li:first-child')
+first.outerHTML='<li>TypeScript</li>'
