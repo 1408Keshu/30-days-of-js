a00843e393baff0367bddce46d426cc9713d182e
keshav31 <keshavchauhangtx2004@gmail.com>
2024-12-22 23:09:07 +0530

    object orientation in javascript

---
+++ Classes_and_Oops/notes.md
@@ -0,0 +1,21 @@
+# JavaScript snd classes
+## OOP
+## Object
+-collection of properties and methods
+- toLowerCase
+
+## why use OOP
+
+## parts of OOP
+Object literal
+
+-Constructor function
+-prototypes
+-classes
+-instances(new,this)
+
+## 4 Pillars
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
---
+++ Classes_and_Oops/oop.js
@@ -0,0 +1,36 @@
+const user={
+    userName:"Keshav",
+    loginCount:8,
+    signIn:true,
+    getUserDetails:function(){
+        console.log("Got user details from database")
+        console.log(`username: ${this.userName}`)
+        console.log(this)
+        // use of this => current Context
+
+    }
+}
+// console.log(user.userName)
+// console.log(user.signIn)
+// console.log(user.getUserDetails())
+// console.log(this)
+
+// new is instance constructor
+// const promiseOne=new Promise()
+// const date=new Date();
+function User(username,loginCount,isLoggedIn){
+    this.username=username
+    this.loginCount=loginCount
+    this.isLoggedIn=isLoggedIn
+// leftside  variable |right side jo pass kr ke de rahe ho
+    return this
+}
+// const userOne=User("keshav",12,true);
+// const userTwo=User("Shubham",32,false);
+// userTwo will override userOne 
+// so we user new keyWord
+const userOne=new User("keshav",12,true);
+const userTwo=new User("Shubham",32,false);
+
+console.log(userOne.username);
+console.log(userTwo.username);
