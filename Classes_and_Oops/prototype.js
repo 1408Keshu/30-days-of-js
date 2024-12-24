b53e3f5658616b3095416320e5df1e2c4b6bd1fb
keshav31 <keshavchauhangtx2004@gmail.com>
2024-12-24 20:35:32 +0530

    protype in js

--- Classes_and_Oops/prototype.js
+++ Classes_and_Oops/prototype.js
@@ -1,7 +1,7 @@
 let myName="keshav"
 let mychannel="Korplz"
 
-console.log(myName.trueLength);
+// console.log(myName.trueLength);
 let myHeros=["thor","spiderman"]
 
 let heroPower={
@@ -14,5 +14,39 @@ let heroPower={
 Object.prototype.keshav=function(){
     console.log(`keshav is present in all hearts`)
 }
-heroPower.getSpiderPower()
-heroPower.keshav()
+Array.prototype.heyKeshav=function(){
+    console.log(`Keshav says Hello`)
+}
+// heroPower.getSpiderPower()
+// heroPower.keshav()
+// heroPower.heykeshav()
+
+// prototype inheritance
+const User={
+name:"badal",
+email:"badli@google.com"
+}
+const Teacher={
+    makeVideo:true
+}
+const TeachingSupport={
+    isAvailable:false
+}
+const TaSupport={
+    makeAssignment:'JS assignment',
+    fullTime:true,
+    __proto__:TeachingSupport
+}
+Teacher.__proto__=User
+
+//modern syntax
+Object.setPrototypeOf(TeachingSupport,Teacher)
+
+let anotherUsername="KaluKaluti     "
+String.prototype.trueLength=function(){
+    console.log(`${this}`); //refereneve to KaluKaluti
+    console.log(`True length is: ${this.trim().length}`);
+}
+anotherUsername.trueLength();
+"keshav".trueLength();
+"IceTea".trueLength();
